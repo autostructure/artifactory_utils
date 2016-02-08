d915187776e09980a6a8cfe89ce55581ad58da33
Bryan Belanger <bbelanger@azcender.com>
2016-02-08 09:35:03 -0500

    Fix repository sync

--- lib/puppet/type/repository_sync:linux.rb
+++
@@ -1,4 +0,0 @@
-Puppet::Type.type(:repository_sync).provide :linux do
-  desc "User management via `useradd` and its ilk.  Note that you will need to
-    install Ruby's shadow password library (often known as `ruby-libshadow`)
-    if you wish to manage user passwords."
