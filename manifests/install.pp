bc248c59625ed91175440147dee377b2ab72cdb3
Bryan Belanger <bbelanger@azcender.com>
2016-02-11 07:53:42 -0500

    Fix util pre-reqs

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,4 +7,23 @@ class artifactory_utils::install {
   package { $::artifactory_utils::package_name:
     ensure => present,
   }
+
+  # Requires some gems
+  package {'rest-client':
+    ensure   => present,
+    provider => 'puppet_gem',
+    require  => [ Package['gcc'], Package['ruby-devel'], Package['rubygem'] ],
+  }
+
+  package { 'gcc':
+    ensure => present,
+  }
+
+  package { 'ruby-devel':
+    ensure => present,
+  }
+
+  package { 'rubygem':
+    ensure => present,
+  }
 }
