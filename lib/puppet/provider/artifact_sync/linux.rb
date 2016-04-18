da0ef4bc66db02e4525dc64bd29b39be30f49cbd
Bryan Belanger <bbelanger@azcender.com>
2016-04-18 14:10:32 -0400

    Use open-uri
    Use open-uri
    
    Use open-uri
    
    change sha1 test to http header
    
    change sha1 test to http header
    
    change sha1 test to http header
    
    change sha1 test to http header
    
    Go back to net http for header only
    
    Go back to net http for header only
    
    Go back to net http for header only
    
    Go back to net http for header only
    
    Go back to net http for header only

--- lib/puppet/provider/artifact_sync/linux.rb
+++ lib/puppet/provider/artifact_sync/linux.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'open-uri'
 
 # Synchronized an artifatory repository by name to a destination
 Puppet::Type.type(:artifact_sync).provide :linux do
@@ -6,32 +7,42 @@ Puppet::Type.type(:artifact_sync).provide :linux do
 
   defaultfor :osfamily => :RedHat
 
-  def get_query(source_url,  user_name, password_hash)
+  def get_header(source_url,  user_name, password_hash)
     # [RELEASE] has special signifiance in Artifactory. Let's escape it
-     url = source_url.gsub(/\[RELEASE\]/, '%5BRELEASE%5D')
+    url = source_url.gsub(/\[RELEASE\]/, '%5BRELEASE%5D')
 
     uri_get = URI.parse(url)
-    http_get = Net::HTTP.new(uri_get.host, uri_get.port)
+    http = Net::HTTP.start(uri_get.host, uri_get.port)
 
-    request_get = Net::HTTP::Get.new(uri_get.request_uri)
+    response = http.head(uri_get.request_uri)
+
+    if response.code == 301
+      response = get_query(URI.parse(response.header['location']), user_name, password_hash)
+    end
+
+    return response
+  end
+
+
+  def get_query(source_url,  user_name, password_hash)
+    # [RELEASE] has special signifiance in Artifactory. Let's escape it
+    url = source_url.gsub(/\[RELEASE\]/, '%5BRELEASE%5D')
 
     # user and password_hash are here use auth
     if user_name and password_hash
-      request_get.basic_auth user_name, password_hash
+      return open(url, http_basic_authentication: [user_name, password_hash])
+    else
+      return open(url)
     end
-
-    response = http_get.request(request_get)
-
-    return response
   end
 
   # Write a new file to the destination
   def write_file(url, destination, user_name, password_hash)
     response = get_query(url, user_name, password_hash)
 
-    if response.code == "200"
+    if response.status[0] == "200"
       open(destination, 'wb') do |file|
-        file.write(response.body)
+        file.write(response.read)
       end
     else
       raise Puppet::Error, 'No file can be found at ' + url
@@ -59,16 +70,7 @@ Puppet::Type.type(:artifact_sync).provide :linux do
     when 'absent'
       return true
     else
-      # Separate the path to access storage api
-      source_components = source_url.scan(/(.+\/artifactory)\/(.+)$/)[0]
-
-      artifactory_path = source_components[0]
-      war_path  = source_components[1]
-
-      # File info URL
-      file_info = artifactory_path + '/api/storage/' + war_path
-
-      response  = get_query(file_info, user, password)
+      response = get_header(source_url, user, password)
 
       # We should only move forward on a 200
       if response.code == "401"
@@ -77,7 +79,8 @@ Puppet::Type.type(:artifact_sync).provide :linux do
         raise Puppet::Error, 'No file can be found at ' + source_url
       end
 
-      current_sha1 =  JSON.parse(response.body)['checksums']['sha1']
+      # Checksum returned by the http response
+      current_sha1 = response['x-checksum-sha1']
 
       # Compute digest for a file
       sha1 = Digest::SHA1.file destination
