a8df72eaf56b4e9bcdfcf58d8ecd6e223ce1d3a2
Bryan Belanger <bryan@Bryans-Mac.local>
2016-08-27 22:45:50 -0400

    First basic test

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,25 +1,35 @@
 require 'spec_helper_acceptance'
 
-describe 'artifactory_utils class' do
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'should work idempotently with no errors' do
-      pp = <<-EOS
-      class { 'artifactory_utils': }
-      EOS
+describe 'artifactory_utils::artifact_sync' do
+  let(:title) { '/tmp/my_app.war' }
 
-      # Run it twice and test for idempotency
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
+  let(:params) {
+    {
+      ensure: 'present',
+      source_url: 'http://artifactory.azcender.com/artifactory/temp/org/apache/sample/1.0.0/sample-1.0.0.war'
+    }
+  }
 
-    describe package('artifactory_utils') do
-      it { is_expected.to be_installed }
-    end
-
-    describe service('artifactory_utils') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+      end
     end
   end
+
+  # context 'default parameters' do
+  #   # Using puppet_apply as a helper
+  #   it 'should work idempotently with no errors' do
+  #     pp = <<-EOS
+  #     class { 'artifactory_utils': }
+  #     EOS
+
+  #     # Run it twice and test for idempotency
+  #     apply_manifest(pp, catch_failures: true)
+  #     apply_manifest(pp, catch_changes: true)
+  #   end
+  # end
 end
