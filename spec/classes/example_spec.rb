d47bb2656aa2f9cc7409d78945d259e36a4f8a27
Bryan Belanger <bryan@Bryans-Mac.local>
2016-08-28 08:25:08 -0400

    MOve test

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
+describe 'artifactory_utils::artifact_sync' do
+  let(:title) { '/tmp/my_app.war' }
+
+  let(:params) {
+    {
+      ensure: 'present',
+      source_url: 'http://artifactory.azcender.com/artifactory/temp/org/apache/sample/1.0.0/sample-1.0.0.war'
+    }
+  }
+
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        File.exist? '/tmp/my_app.war'
+        File.exist? '/tmp/my_app.warx'
+      end
+    end
+  end
+end
+
 # describe 'artifactory_utils' do
 #   context 'supported operating systems' do
 #     on_supported_os.each do |os, facts|
